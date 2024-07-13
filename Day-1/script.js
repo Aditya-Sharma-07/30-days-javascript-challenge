aea96358e8e094c8e0142202d44dbd981be9cb9a
Aditya Sharma <inspiring.passage@gmail.com>
2024-07-13 07:36:06 +0530

    day-1 completed

---
+++ Day-1/script.js
@@ -0,0 +1,36 @@
+// Activity 1 : Variable declaration
+var num = 13;
+console.log(num);
+let str = "Coding";
+console.log(str);
+
+// Activity 2 : Const declaration
+const guess = true;
+console.log(guess);
+
+// Acitivity 3 : Data types
+let Num = 11;
+let Str = "Script";
+let Guess = false;
+let obj = {
+    id: 121,
+    pass: 2717
+};
+let arr = [1,2,"b"];
+
+console.log(typeof Num);
+console.log(typeof Str);
+console.log(typeof Guess);
+console.log(typeof obj);
+console.log(typeof arr);
+
+// Activity 4 : Reassigning variables
+let num2 = 201;
+console.log(`Before: ${num2}`);
+num2 = 111;
+console.log(`After: ${num2}`);
+
+// Activity 5: Understanding const
+const perm = "an";
+// perm = "a"; // can't resign const variable
+console.log(perm);
