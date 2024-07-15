cb4116154821557c887500868403d36b0af1773e
Aditya Sharma <inspiring.passage@gmail.com>
2024-07-15 10:20:25 +0530

    day-3 updated

---
+++ Day-3/act-2.js
@@ -0,0 +1,12 @@
+// Activity 2
+let num1 = 2;
+let num2 = 3;
+let num3 = 5;
+
+if (num1 >= num2 && num1 >= num3) {
+    console.log(`${num1} is the largest number`);
+} else if (num2 >= num1 && num2 >= num3) {
+    console.log(`${num2} is the largest number`);
+} else if (num3 >= num1 && num3 >= num2) {
+    console.log(`${num3} is the largest number`);
+}
---
+++ Day-3/act-4.js
@@ -0,0 +1,3 @@
+// Activity 4
+let toBeChecked = 19;
+toBeChecked % 2 == 0 ? console.log("Even Number"):console.log("Odd Number");
