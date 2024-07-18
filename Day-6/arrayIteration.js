e70219d34f571d99d070385025de0f5424ff71cd
Aditya Sharma <inspiring.passage@gmail.com>
2024-07-18 13:03:38 +0530

    day-6 updated

---
+++ Day-6/arrayIteration.js
@@ -0,0 +1,11 @@
+// Activity 4: Array Iteration
+// Task 10: Use a for loop to iterate over the array and log each element to the console.
+let evenArr = [2,4,6,8];
+let arr = [21,41,2,3];
+for (let index = 0; index < evenArr.length; index++) {
+    console.log(evenArr[index]);
+}
+// Task 11: Use the forEach method to iterate over the array and log each element to the console.
+arr.forEach((e) => {
+    console.log(e);
+});
---
+++ Day-6/twoDimensionalArray.js
@@ -0,0 +1,6 @@
+let matrix = [[1,11],[2,22],[3,33]];
+console.log(matrix);
+
+console.log(matrix[2][0]);
+matrix[2][0] = 9;
+console.log(matrix[2][0]);
